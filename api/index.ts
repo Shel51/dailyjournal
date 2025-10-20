bc61abbf2ad8a654682824ef407e89b62b959609
Shel51 <shailimodi@gmail.com>
2025-10-20 17:17:26 +0530

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,1 +1,13 @@
+import serverless from "serverless-http";
+import { createApp } from "../server/app";
+
+let cachedHandler: any;
+
+export default async function handler(req: any, res: any) {
+  if (!cachedHandler) {
+    const app = await createApp();
+    cachedHandler = serverless(app);
+  }
+  return cachedHandler(req, res);
+}
 
